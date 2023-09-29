66ef4ffa9a88d88cf372c4fe510471577c8f3407
isabeladavantel <110184962+isabeladavantel@users.noreply.github.com>
2023-09-29 08:10:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-clicado = () => alert("aloo mundooo");
-mensagem  = (msg) => console.log(msg);
-soma =(a,b) => a + b;
-sub =(a,b) => a - b;
-mult =(a,b) => a * b;
-div =(a,b) => a / b;
-juros_simples = (c,j) => mult(c.div(j,100));
-
-
-mensagem(soma(15,soma(11, soma(4,7))));
-
-mensagem(sub(18,7));
-
-mensagem(soma(2,div(2,2)))
-
-mensagem(juros_simples(428,18))
+let num1 ="";
+let num2 ="";
+let valor ="";
+let temPonto =
+mensagem = (msg) => console.log(msg);
+soma = (a,b) => a + b;
+sub = (a,b) => a - b;
+mult = (a,b) => a * b;
+div = (a,b) => a / b;
+juros_simples = (c,j) => mult(c,div(j,100));
+montante_juro_simples = (c,j) => soma(juros_simples(c,j),c);
+ 
+mostrar_display = (msg) =>{
+  document.getElementById("reultado").value = msg;
+}
+function digitando (tecla){
+  if(tecla =="."){
+    if(!temPonto){
+      valor += tecla;
+      mostrar_display(valor);
+      temPonto = true;
+    }
+    return;
+  }
+  valor += tecla;
+  mostrar_display(valor);
+}
